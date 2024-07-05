4d279838b24d3bcdb7b28e04d56e4cbe4032ce4f
Edson Paul Olimberio <edsonpaul98@gmail.com>
2024-07-05 10:54:02 +0800

    Test complete

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,36 @@ def search_users(args):
     """
 
     # Implement search here!
+    results_set = set()
+    
+    search_id = args.get("id")
+    search_name = args.get("name")
+    search_age = args.get("age")
+    search_occupation = args.get("occupation")
 
-    return USERS
+    if search_id:
+        for user in USERS:
+            if str(user["id"]) == search_id:
+                results_set.add(tuple(user.items()))
+                break
+    for user in USERS:
+        if search_id and str(user["id"]) == search_id:
+            continue
+        if search_name and search_name.lower() in user["name"].lower():
+            results_set.add(tuple(user.items()))
+        if search_age:
+            age = int(search_age)
+            if age - 1 <= user["age"] <= age + 1:
+                results_set.add(tuple(user.items()))
+        if search_occupation and search_occupation.lower() in user["occupation"].lower():
+            results_set.add(tuple(user.items()))
+
+
+    results = [dict(user) for user in results_set]
+
+
+    if not search_id and not search_name and not search_age and not search_occupation:
+        return USERS
+
+    return results
+
